93fe9fc1495875fda9f54b039c7c9db79f9cdd0b
CarnunMP <carnun@hotmail.co.uk>
2019-12-17 08:17:27 +0000

    getRelatives in broken state; about to re-do

--- rest-api/recipe/models/recipe-models.js
+++ rest-api/recipe/models/recipe-models.js
@@ -30,49 +30,73 @@ async function getHistory(id) {
 }
 
 async function getRelatives(id, order, max_length = null) {
-  let relatives = await getImmediateRelatives(id);
-  let passes = 1;
+  // let relatives = [];
+  // let toVisit = await getImmediateRelatives(id);
 
-  while (passes <= order) {
-    relatives.forEach(async relative => {
-      const newRelatives = await getImmediateRelatives(relative.id);
-      mergeSortedRecipeArrays(relatives, newRelatives);
-    });
+  // let passes = 1;
+  // while (passes <= order) {
+  //   for (let i = 0; i < toVisit.length; i++) {
+  //     const currentRecipe = toVisit.shift();
+  //     const newRecipes = await getImmediateRelatives(currentRecipe.id);
 
-    passes++;
-  }
+  //     if (!includesRecipe(relatives, currentRecipe)) {
+  //       toVisit.push(currentRecipe);
+  //     }
 
-  return max_length? relatives.slice(0, max_length) : relatives;
+  //     relatives = mergeSortedRecipeArrays(relatives, newRecipes);
+  //   }
+
+  //   passes++;
+  // }
+
+  // return max_length? relatives.slice(0, max_length) : relatives;
 }
 
 // getRelatives helper
-async function getImmediateRelatives(id) {
-  const currentRecipe = await findById(id);
-  const parent = await findById(currentRecipe.parent_id);
-  const children = await db('recipes').where({ 'parent_id': id });
+// async function getImmediateRelatives(id) {
+//   const currentRecipe = await findById(id);
+//   const parent = await findById(currentRecipe.parent_id);
+//   const children = await db('recipes').where({ 'parent_id': id });
 
-  return [parent, ...children].sort((id1, id2) => id1 - id2);
-}
+//   return [parent, ...children].sort((recipe1, recipe2) => recipe1.id - recipe2.id);
+// }
 
 // getRelatives helper
-function mergeSortedRecipeArrays(arr1, arr2) {
-  let i = 0;
-  let j = 0;
-  let newArray = [];
+// function mergeSortedRecipeArrays(recipeArray1, recipeArray2) {
+//   let i = 0;
+//   let j = 0;
+//   let newArray = [];
 
-  while (i < arr1.length || j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      newArray.push(arr1[i]);
-      i++;
-    } else if (arr2[j] < arr1[i]) {
-      newArray.push(arr2[j]);
-      j++;
-    } else {
-      newArray.push(arr1[i]);
-      i++;
-      j++;
-    }
-  }
+//   while (i < recipeArray1.length || j < recipeArray2.length) {
+//     if (recipeArray1.length === 0) {
+//       newArray = recipeArray2;
+//       j = recipeArray2.length;
+//     } else if (recipeArray2.length === 0) {
+//       newArray = recipeArray1;
+//       i = recipeArray2.length;
+//     } else if (recipeArray1[i].id < recipeArray2[j].id) {
+//       newArray.push(recipeArray1[i]);
+//       i++;
+//     } else if (recipeArray2[j].id < recipeArray1[i].id) {
+//       newArray.push(recipeArray2[j]);
+//       j++;
+//     } else {
+//       newArray.push(recipeArray1[i]);
+//       i++;
+//       j++;
+//     }
+//   }
 
-  return newArray;
-}
+//   return newArray;
+// }
+
+// getRelativesHelper
+// function includesRecipe(arr, recipe) {
+//   arr.forEach(r => {
+//     if (r.id === recipe.id) {
+//       return true;
+//     }
+//   });
+
+//   return false;
+// }
