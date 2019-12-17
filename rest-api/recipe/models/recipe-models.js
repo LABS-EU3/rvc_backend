17a76a0737400364df8809a3c2bb8c30adae975c
CarnunMP <carnun@hotmail.co.uk>
2019-12-17 16:07:51 +0000

    weaker version of getRelatives (getFirstOrderRelatives) written and tested

--- rest-api/recipe/models/recipe-models.js
+++ rest-api/recipe/models/recipe-models.js
@@ -3,7 +3,7 @@ const db = require('../../../test-database/db-config');
 module.exports = {
   find,
   getHistory,
-  getRelatives
+  getFirstOrderRelatives
 }
 
 function find() {
@@ -29,74 +29,15 @@ async function getHistory(id) {
   return history;
 }
 
-async function getRelatives(id, order, max_length = null) {
-  // let relatives = [];
-  // let toVisit = await getImmediateRelatives(id);
+// Note: writing a more general algorithm than the below has proven difficult, so this will have to do for now!
+// I fully intend to come back to it in my own time, though, as I feel I'm relatively close to a solution.
+// Watch this space!
+async function getFirstOrderRelatives(id, max_length = null) {
+  const currentRecipe = await findById(id);
+  const parent = await findById(currentRecipe.parent_id);
+  const children = await db('recipes').where({ 'parent_id': id }).andWhereNot({ 'id': currentRecipe.id });
 
-  // let passes = 1;
-  // while (passes <= order) {
-  //   for (let i = 0; i < toVisit.length; i++) {
-  //     const currentRecipe = toVisit.shift();
-  //     const newRecipes = await getImmediateRelatives(currentRecipe.id);
+  const firstOrderRelatives = (id === parent.id) ? children : [parent, ...children];
 
-  //     if (!includesRecipe(relatives, currentRecipe)) {
-  //       toVisit.push(currentRecipe);
-  //     }
-
-  //     relatives = mergeSortedRecipeArrays(relatives, newRecipes);
-  //   }
-
-  //   passes++;
-  // }
-
-  // return max_length? relatives.slice(0, max_length) : relatives;
+  return max_length ? firstOrderRelatives.slice(0, max_length) : firstOrderRelatives;
 }
-
-// getRelatives helper
-// async function getImmediateRelatives(id) {
-//   const currentRecipe = await findById(id);
-//   const parent = await findById(currentRecipe.parent_id);
-//   const children = await db('recipes').where({ 'parent_id': id });
-
-//   return [parent, ...children].sort((recipe1, recipe2) => recipe1.id - recipe2.id);
-// }
-
-// getRelatives helper
-// function mergeSortedRecipeArrays(recipeArray1, recipeArray2) {
-//   let i = 0;
-//   let j = 0;
-//   let newArray = [];
-
-//   while (i < recipeArray1.length || j < recipeArray2.length) {
-//     if (recipeArray1.length === 0) {
-//       newArray = recipeArray2;
-//       j = recipeArray2.length;
-//     } else if (recipeArray2.length === 0) {
-//       newArray = recipeArray1;
-//       i = recipeArray2.length;
-//     } else if (recipeArray1[i].id < recipeArray2[j].id) {
-//       newArray.push(recipeArray1[i]);
-//       i++;
-//     } else if (recipeArray2[j].id < recipeArray1[i].id) {
-//       newArray.push(recipeArray2[j]);
-//       j++;
-//     } else {
-//       newArray.push(recipeArray1[i]);
-//       i++;
-//       j++;
-//     }
-//   }
-
-//   return newArray;
-// }
-
-// getRelativesHelper
-// function includesRecipe(arr, recipe) {
-//   arr.forEach(r => {
-//     if (r.id === recipe.id) {
-//       return true;
-//     }
-//   });
-
-//   return false;
-// }
--- rest-api/recipe/models/recipe-models.spec.js
+++ rest-api/recipe/models/recipe-models.spec.js
@@ -25,34 +25,72 @@ describe('recipes-model', () => {
     });
   });
 
-  describe('getRelatives(id, order, maxLength)', () => {
-    it('should return a sorted array of first-order relatives', async () => {      
-      const expected1stOrderRelativesOfRecipeTwo = [
+  describe('getFirstOrderRelatives(id, maxLength)', () => {
+    it('should return an array of first-order relatives', async () => {
+      const expectedFirstOrderRelativesOfRecipeTwo = [
         {id: 1, name: 'sweet crepes', parent_id: 1},
         {id: 3, name: 'blueberry pancakes', parent_id: 2},
         {id: 5, name: 'chocolate chip pancakes', parent_id: 2},
         {id: 6, name: 'banana pancakes', parent_id: 2}
       ];
 
-      const actual1stOrderRelativesOfRecipeTwo = await Recipes.getRelatives(2, 1);
+      const actualFirstOrderRelativesOfRecipeTwo = await Recipes.getFirstOrderRelatives(2);
 
-      expect(actual1stOrderRelativesOfRecipeTwo).toEqual(expected1stOrderRelativesOfRecipeTwo);
+      expect(actualFirstOrderRelativesOfRecipeTwo).toEqual(expectedFirstOrderRelativesOfRecipeTwo);
     });
 
-    it('should return a sorted array of second-order relatives', async () => {
-      const expected2ndOrderRelativesOfRecipeTwo = [
+    it('should return an array with the specified max_length', async () => {
+      const expectedFirstTwoFirstOrderRelativesOfRecipeTwo = [
         {id: 1, name: 'sweet crepes', parent_id: 1},
         {id: 3, name: 'blueberry pancakes', parent_id: 2},
-        {id: 4, name: 'mini pancakes', parent_id: 1},
-        {id: 5, name: 'chocolate chip pancakes', parent_id: 2},
-        {id: 6, name: 'banana pancakes', parent_id: 2},
-        {id: 8, name: 'wild berry pancakes', parent_id: 3},
-        {id: 9, name: 'savoury crepes', parent_id: 1}
       ];
 
-      const actual2ndOrderRelativesOfRecipeTwo = await Recipes.getRelatives(2, 2);
+      const actualFirstTwoFirstOrderRelativesOfRecipeTwo = await Recipes.getFirstOrderRelatives(2, 2);
 
-      expect(actual2ndOrderRelativesOfRecipeTwo).toEqual(expected2ndOrderRelativesOfRecipeTwo);
+      expect(actualFirstTwoFirstOrderRelativesOfRecipeTwo).toEqual(expectedFirstTwoFirstOrderRelativesOfRecipeTwo);
     });
-  })
+
+    it('should exclude parents if the given recipe is a root', async () => {
+      const expectedFirstOrderRelativesOfRecipeOne = [
+        {id: 2, name: 'pancakes', parent_id: 1},
+        {id: 4, name: 'mini pancakes', parent_id: 1},
+        {id: 9, name: 'savoury crepes', parent_id: 1},
+      ];
+
+      const actualFirstOrderRelativesOfRecipeOne = await Recipes.getFirstOrderRelatives(1);
+
+      expect(actualFirstOrderRelativesOfRecipeOne).toEqual(expectedFirstOrderRelativesOfRecipeOne);
+    });
+  });
+
+  // describe('getRelatives(id, order, maxLength)', () => {
+  //   it('should return a sorted array of first-order relatives', async () => {      
+  //     const expected1stOrderRelativesOfRecipeTwo = [
+  //       {id: 1, name: 'sweet crepes', parent_id: 1},
+  //       {id: 3, name: 'blueberry pancakes', parent_id: 2},
+  //       {id: 5, name: 'chocolate chip pancakes', parent_id: 2},
+  //       {id: 6, name: 'banana pancakes', parent_id: 2}
+  //     ];
+
+  //     const actual1stOrderRelativesOfRecipeTwo = await Recipes.getRelatives(2, 1);
+
+  //     expect(actual1stOrderRelativesOfRecipeTwo).toEqual(expected1stOrderRelativesOfRecipeTwo);
+  //   });
+
+  //   it('should return a sorted array of second-order relatives', async () => {
+  //     const expected2ndOrderRelativesOfRecipeTwo = [
+  //       {id: 1, name: 'sweet crepes', parent_id: 1},
+  //       {id: 3, name: 'blueberry pancakes', parent_id: 2},
+  //       {id: 4, name: 'mini pancakes', parent_id: 1},
+  //       {id: 5, name: 'chocolate chip pancakes', parent_id: 2},
+  //       {id: 6, name: 'banana pancakes', parent_id: 2},
+  //       {id: 8, name: 'wild berry pancakes', parent_id: 3},
+  //       {id: 9, name: 'savoury crepes', parent_id: 1}
+  //     ];
+
+  //     const actual2ndOrderRelativesOfRecipeTwo = await Recipes.getRelatives(2, 2);
+
+  //     expect(actual2ndOrderRelativesOfRecipeTwo).toEqual(expected2ndOrderRelativesOfRecipeTwo);
+  //   });
+  // })
 })
