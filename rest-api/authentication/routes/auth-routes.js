8d98fbfe771083cf7682f292d919a7d08991a823
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-12-17 14:38:29 +0100

    use dataType validator middlewares

--- rest-api/authentication/routes/auth-routes.js
+++ rest-api/authentication/routes/auth-routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const { register, login} = require('../controllers/auth-controller');
 
 const { hashPassword } = require('../middlewares/hashPassword')
-const { validateToken } = require('../middlewares/validateToken')
+const { validateFields } = require('../middlewares/validateFields')
+const { validateEmail } = require('../middlewares/validateEmail')
 
 
-router.post('/register', hashPassword, register);
-router.post('/login', login)
+
+router.post('/register', validateFields, validateEmail, hashPassword, register);
+router.post('/login', validateFields, validateEmail, login)
 
 module.exports = router;
