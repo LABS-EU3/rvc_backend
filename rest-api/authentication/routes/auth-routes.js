105d66595ea40833610d6996ee03ac63cf9bbeaa
Patrick Rodrigues <47316946+rodpa715@users.noreply.github.com>
2019-12-17 18:22:55 +0100

    add middlewares

--- rest-api/authentication/routes/auth-routes.js
+++ rest-api/authentication/routes/auth-routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
-const { register, login} = require('../controllers/auth-controller');
+const { register, login } = require('../controllers/auth-controller');
 
 const { hashPassword } = require('../middlewares/hashPassword')
 const { validateFields } = require('../middlewares/validateFields')
 const { validateEmail } = require('../middlewares/validateEmail')
+const { checkExistingEmail } = require('../middlewares/checkExistingEmail')
+const { checkExistingUsername } = require('../middlewares/checkExistingUsername')
 
 
-
-router.post('/register', validateFields, validateEmail, hashPassword, register);
+router.post('/register', validateFields, validateEmail, checkExistingEmail, checkExistingUsername, hashPassword, register);
 router.post('/login', validateFields, validateEmail, login)
 
 module.exports = router;
