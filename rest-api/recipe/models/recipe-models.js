a130f68b154019a1a9fa3767aed99d36f5d5c8fd
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-08 10:04:36 +0100

    add monolith transaction

--- rest-api/recipe/models/recipe-models.js
+++ rest-api/recipe/models/recipe-models.js
@@ -70,4 +70,124 @@ async function getRecipeById(id) {
   return { ...recipe, instructions, ingredients, tags, };
 }
 
+async function addRecipeTransaction(body) {
+  const transaction = await db.transaction(async trx => {
+    try {
+      // body.recipes === object
+      // body.instructions === array
+      // body.tags === array
+      // body.recipe_tags === array
+      // body.images === array
+      // body.ingredients === array
+      // body.recipe_ingredients === array
+      // body.recipe_categories === array
 
+
+      // RECIPES
+      const [recipe] = await db('recipes').insert(body.recipes).returning("*");
+
+      // INSTRUCTIONS
+      const instructions = await db('instructions')
+        .insert(body.instructions)
+        .returning('*');
+
+      // The reason for the mapping is because you need to have the id's of the added instructions
+      // in order to link insert them into the intermediary table
+
+      const recipe_instructions_object = instructions.map(instruction => ({
+        instruction_id: instruction.id,
+        recipe_id: recipe.id
+      }));
+
+      const recipe_instructions = await db('recipe_instructions').insert(
+        recipe_instructions_object
+      );
+
+      // TAGS
+      // The reason why it checks for a tag, is because the user has the ability to add new tags
+      // However, if the tags already exists we then expect a recipe_tags object with the provided id's
+      if (body.tags) {
+        const tags = await trx('tags')
+          .insert(body.tags)
+          .returning('*');
+
+        const recipe_tags_object = tags.map(tag => ({
+          tag_id: tag.id,
+          recipe_id: recipe.id
+        }));
+
+        const recipe_tags = await trx('recipe_tags').insert(recipe_tags_object);
+      } else {
+        // The reason for this map is because we need to have the id of the newly created recipe
+        const recipe_tags_object = body.recipe_tags.map(tag => {
+          return { ...tag, recipe_id: recipe.id };
+        });
+        const recipes_tags = await trx('tags').insert(recipe_tags_object);
+      }
+
+      // IMAGES
+      const images = await trx('images')
+        .insert(body.images)
+        .returning('*');
+
+      const recipe_images_object = images.map(img => ({
+        image_id: img.id,
+        recipe_id: recipe.id
+      }));
+
+      const recipe_images = await trx('recipe_images').insert(
+        recipe_images_object
+      );
+
+      // INGREDIENTS
+
+      if (body.ingredients) {
+
+        const ingredients = await trx('ingredients')
+          .insert(body.ingredients)
+          .returning('*');
+
+        const recipe_ingredients_object = ingredients.map((ingredient, index) => {
+          return { ...body.recipe_ingredients[index], ingredient_id: ingredient.id, recipe_id: recipe.id };
+        });
+
+        const recipe_ingredients = await trx('recipe_ingredients').insert(
+          recipe_ingredients_object
+        );
+      } else {
+        const recipe_ingredients_object = body.recipe_ingredients.map(ingredient => {
+          return {
+            ...ingredient,
+            recipe_id: recipe.id
+          };
+        });
+
+        const recipe_ingredients = await trx('recipe_ingredients').insert(
+          recipe_ingredients_object
+        );
+      }
+
+      // RECIPE CATEGORIES
+      // TO BE CHANGED, CATEGORIES SHOULD NOT BE ADDED THIS IS FOR TESTING ONLY
+      // SINCE THERE ARE NO SEEDS
+      const categories = await trx("categories")
+        .insert(body.categories)
+        .returning("*")
+
+      const recipe_categories_object = categories.map(category => {
+        return {
+          category_id: category.id,
+          recipe_id: recipe.id
+        };
+      });
+
+      const recipe_categories = await trx("recipe_categories").insert(recipe_categories_object)
+
+      return recipe.id
+    } catch (error) {
+      throw error;
+    }
+  })
+
+  return getRecipeById(transaction)
+}
