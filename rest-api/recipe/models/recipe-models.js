51f26dd46ca54648b54ef2b844d66e417c35574f
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-08 10:04:21 +0100

    remove aliases from knex queries

--- rest-api/recipe/models/recipe-models.js
+++ rest-api/recipe/models/recipe-models.js
@@ -2,61 +2,61 @@ const db = require('../../../database/dbConfig');
 
 module.exports = {
   getRecipes,
-  getRecipeById
+  getRecipeById,
+  addRecipeTransaction
 };
 
 async function getRecipes() {
-  const recipes = await db('recipes as r')
-    .leftJoin('recipe_images as rim', 'rim.recipe_id', 'r.id')
-    .leftJoin('images as i', 'i.id', 'rim.image_id')
+  const recipes = await db('recipes')
+    .leftJoin('recipe_images', 'recipe_images.recipe_id', 'recipes.id')
+    .leftJoin('images', 'images.id', 'recipe_images.image_id')
     .select(
-      'r.id',
-      'r.title as recipe_title',
-      'r.description',
-      'r.time_required',
-      'r.difficulty',
-      'r.budget',
-      'i.url as image'
+      'recipes.id',
+      'recipes.title as recipe_title',
+      'recipes.description',
+      'recipes.time_required',
+      'recipes.difficulty',
+      'recipes.budget',
+      'images.url as imageUrl'
     );
   return recipes;
 }
 
 async function getRecipeById(id) {
-  const recipe = await db('recipes as r')
-    .leftJoin('recipe_images as rim', 'rim.recipe_id', 'r.id')
-    .leftJoin('images as i', 'i.id', 'rim.image_id')
-    .leftJoin('recipe_categories as rc', 'rc.recipe_id', 'r.id')
-    .leftJoin('categories as c', 'c.id', 'rc.category_id')
+  const recipe = await db('recipes')
+    .leftJoin('recipe_images', 'recipe_images.recipe_id', 'recipes.id')
+    .leftJoin('images', 'images.id', 'recipe_images.image_id')
+    .leftJoin('recipe_categories', 'recipe_categories.recipe_id', 'recipes.id')
+    .leftJoin('categories', 'categories.id', 'recipe_categories.category_id')
     .select(
-      'r.id',
-      'r.title as recipe_title',
-      'r.description',
-      'r.time_required',
-      'r.difficulty',
-      'r.budget',
-      'i.url as image',
-      'c.name as category'
+      'recipes.id',
+      'recipes.title as recipe_title',
+      'recipes.description',
+      'recipes.time_required',
+      'recipes.difficulty',
+      'recipes.budget',
+      'images.url as image',
+      'categories.name as category'
     )
-    .where('r.id', id)
+    .where('recipes.id', id)
     .first();
 
-  const ingredients = await db('recipe_ingredients as ri')
-    .join('recipes as r', 'r.id', 'ri.recipe_id')
-    .join('ingredients as i', 'i.id', 'ri.ingredient_id')
-    .join('units as u', 'u.id', 'ri.unit_id')
+  const ingredients = await db('recipe_ingredients')
+    .join('recipes', 'recipes.id', 'recipe_ingredients.recipe_id')
+    .join('ingredients', 'ingredients.id', 'recipe_ingredients.ingredient_id')
+    .join('units', 'units.id', 'recipe_ingredients.unit_id')
     .select(
-      'r.title as recipe',
-      'i.name as ingredient',
-      'ri.quantity',
-      'u.name as unit'
+      'ingredients.name',
+      'recipe_ingredients.quantity',
+      'units.name as unit'
     )
-    .where('r.id', id);
+    .where('recipes.id', id);
 
-  const tags = await db('recipe_tags as rt')
-    .join('recipes as r', 'r.id', 'rt.recipe_id')
-    .join('tags as t', 't.id', 'rt.tag_id')
-    .select('r.title as recipe', 't.name as tag')
-    .where('r.id', id);
+  const tags = await db('recipe_tags')
+    .join('recipes', 'recipes.id', 'recipe_tags.recipe_id')
+    .join('tags', 'tags.id', 'recipe_tags.tag_id')
+    .select('recipes.title', 'tags.name as tag')
+    .where('recipes.id', id);
 
   const instructions = await db('recipe_instructions')
     .join(
@@ -67,5 +67,7 @@ async function getRecipeById(id) {
     .select('instructions.text', 'instructions.id')
     .where('recipe_instructions.recipe_id', id);
 
-  return { recipe, ingredients, tags, instructions };
+  return { ...recipe, instructions, ingredients, tags, };
 }
+
+
