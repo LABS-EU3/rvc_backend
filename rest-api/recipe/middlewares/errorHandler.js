edd023f8bdde973e19c2e986621d868d5e89ef6b
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-09 17:01:05 +0100

    add error handling

--- rest-api/recipe/middlewares/errorHandler.js
+++ rest-api/recipe/middlewares/errorHandler.js
@@ -3,7 +3,7 @@ function errorHandler(error) {
     case '23505':
       const matchedString = error.detail.match(/\(([^()]+)\)/g);
       const cleanString = matchedString[1].replace(/([()])/g, "'");
-      return `The value you entered for ${cleanString} already exists`;
+      return `The value you entered ${cleanString} already exists`;
     case '23502':
       return `No value was entered for '${error.column}'`;
     case '42703':
