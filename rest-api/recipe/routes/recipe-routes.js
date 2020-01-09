08f2332a68e505a01170b8969bcf5fa619076289
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-09 17:01:19 +0100

    add middlewares including authentication

--- rest-api/recipe/routes/recipe-routes.js
+++ rest-api/recipe/routes/recipe-routes.js
@@ -6,9 +6,12 @@ const {
   addRecipe
 } = require('../controllers/recipe-controllers');
 const { validateId } = require('../middlewares/validateID');
+const { requiredFields } = require('../middlewares/requiredFields');
+const { emptyFields } = require('../middlewares/emptyFields');
+const { validateToken } = require("../../authentication/middlewares/validateToken");
 
 router.get('/', getRecipes);
 router.get('/:id', validateId, getRecipeById);
-router.post('/', addRecipe)
+router.post('/', validateToken, requiredFields, emptyFields, addRecipe);
 
 module.exports = router;
