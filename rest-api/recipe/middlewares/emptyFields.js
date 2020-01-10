030a94cec23b5bc3854a2295bea9891d0a449026
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-10 13:52:38 +0100

    add extra checking for two fields

--- rest-api/recipe/middlewares/emptyFields.js
+++ rest-api/recipe/middlewares/emptyFields.js
@@ -3,15 +3,15 @@ function emptyFields(req, res, next) {
     res.status(400).json({ error: 'Recipes is empty' });
   } else if (!req.body.instructions.length) {
     res.status(400).json({ error: 'Instructions is empty' });
-  } else if (!req.body.tags.length) {
+  } else if (req.body.hasOwnProperty("tags") && !req.body.tags.length) {
     res.status(400).json({ error: 'Tags is empty' });
-  } else if (!req.body.recipe_tags.length) {
+  } else if (req.body.hasOwnProperty("recipe_tags") && !req.body.recipe_tags.length) {
     res.status(400).json({ error: 'Recipe_tags is empty' });
   } else if (!req.body.images.length) {
     res.status(400).json({ error: 'Images is empty' });
-  } else if (!req.body.ingredients.length) {
+  } else if (req.body.hasOwnProperty("ingredients") && !req.body.ingredients.length) {
     res.status(400).json({ error: 'Ingredients is empty' });
-  } else if (!req.body.recipe_ingredients.length) {
+  } else if (req.body.hasOwnProperty("recipe_ingredients") && !req.body.recipe_ingredients.length) {
     res.status(400).json({ error: 'Recipe_ingredients is empty' });
   } else if (!req.body.recipe_categories.length) {
     res.status(400).json({ error: 'Recipe_categories is empty' });
