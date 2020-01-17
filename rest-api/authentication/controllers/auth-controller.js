d84349db610d62e88bf9cc4d51aa8c328ac4cf5a
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-17 11:08:39 +0100

    throw err when wrong credentials

--- rest-api/authentication/controllers/auth-controller.js
+++ rest-api/authentication/controllers/auth-controller.js
@@ -17,9 +17,9 @@ async function login(req, res) {
     const result = await auth.login(req.body);
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({
+    res.status(error.status || 500).json({
       message: 'Something went wrong, try again in a few minutes',
-      error
+      error: error.error
     });
   }
 }
