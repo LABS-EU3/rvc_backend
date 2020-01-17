da40e7fc6c7ecd213df171595624ed126d149153
Patrick Rodrigues <contact@rodriguespatrick.com>
2020-01-17 11:15:04 +0100

    change middlewares applied

--- rest-api/recipe/routes/recipe-routes.js
+++ rest-api/recipe/routes/recipe-routes.js
@@ -7,13 +7,13 @@ const {
 } = require('../controllers/recipe-controllers');
 const { validateId } = require('../middlewares/validateID');
 const { requiredFields } = require('../middlewares/requiredFields');
-const { emptyFields } = require('../middlewares/emptyFields');
+const { emptyFields } = require('../middlewares/emptyFields'); // Has to be disabled until frontend supports all of the features
 const {
   validateToken
 } = require('../../authentication/middlewares/validateToken');
 
 router.get('/', getRecipes);
 router.get('/:id', validateId, getRecipeById);
-router.post('/', validateToken,  addRecipe);
+router.post('/', validateToken, requiredFields, addRecipe);
 
 module.exports = router;
